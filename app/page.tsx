45a16290f1fcc5bad67f54161d93fc1b65b4cf78
KoniChivaJS <mredik200@gmail.com>
2024-07-26 23:37:08 +0300

    Render products + prisma routing

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
-import { Categories } from "@/components/shared/categories";
 import { Container } from "@/components/shared/container";
 import { Filters } from "@/components/shared/filters";
-import { ProductCard } from "@/components/shared/product-card";
-import { SortPopup } from "@/components/shared/sort-popup";
+import { ProductsGroupList } from "@/components/shared/products-group-list";
 import { Title } from "@/components/shared/title";
 import { TopBar } from "@/components/shared/top-bar";
 
@@ -20,6 +18,115 @@ export default function Home() {
           </div>
           <div className="flex-1">
             <div className="flex flex-col gap-16"></div>
+            <ProductsGroupList
+              title="Піци"
+              items={[
+                {
+                  id: 1,
+                  name: "Піца",
+                  imageUrl:
+                    "https://chernivtsi.celentano.delivery/wp-content/uploads/2019/07/bavarska-445x445.jpg",
+                  items: [{ price: 550 }],
+                },
+                {
+                  id: 2,
+                  name: "Піца",
+                  imageUrl:
+                    "https://chernivtsi.celentano.delivery/wp-content/uploads/2019/07/bavarska-445x445.jpg",
+                  items: [{ price: 550 }],
+                },
+                {
+                  id: 3,
+                  name: "Піца",
+                  imageUrl:
+                    "https://chernivtsi.celentano.delivery/wp-content/uploads/2019/07/bavarska-445x445.jpg",
+                  items: [{ price: 550 }],
+                },
+                {
+                  id: 4,
+                  name: "Піца",
+                  imageUrl:
+                    "https://chernivtsi.celentano.delivery/wp-content/uploads/2019/07/bavarska-445x445.jpg",
+                  items: [{ price: 550 }],
+                },
+                {
+                  id: 5,
+                  name: "Піца",
+                  imageUrl:
+                    "https://chernivtsi.celentano.delivery/wp-content/uploads/2019/07/bavarska-445x445.jpg",
+                  items: [{ price: 550 }],
+                },
+              ]}
+              categoryId={1}
+            />
+            <ProductsGroupList
+              title="Комбо"
+              items={[
+                {
+                  id: 6,
+                  name: "Піца",
+                  imageUrl:
+                    "https://chernivtsi.celentano.delivery/wp-content/uploads/2019/07/bavarska-445x445.jpg",
+                  items: [{ price: 550 }],
+                },
+                {
+                  id: 7,
+                  name: "Піца",
+                  imageUrl:
+                    "https://chernivtsi.celentano.delivery/wp-content/uploads/2019/07/bavarska-445x445.jpg",
+                  items: [{ price: 550 }],
+                },
+                {
+                  id: 8,
+                  name: "Піца",
+                  imageUrl:
+                    "https://chernivtsi.celentano.delivery/wp-content/uploads/2019/07/bavarska-445x445.jpg",
+                  items: [{ price: 550 }],
+                },
+              ]}
+              categoryId={2}
+            />
+            <ProductsGroupList
+              title="Закуски"
+              items={[
+                {
+                  id: 9,
+                  name: "Піца",
+                  imageUrl:
+                    "https://chernivtsi.celentano.delivery/wp-content/uploads/2019/07/bavarska-445x445.jpg",
+                  items: [{ price: 550 }],
+                },
+                {
+                  id: 0,
+                  name: "Піца",
+                  imageUrl:
+                    "https://chernivtsi.celentano.delivery/wp-content/uploads/2019/07/bavarska-445x445.jpg",
+                  items: [{ price: 550 }],
+                },
+                {
+                  id: 11,
+                  name: "Піца",
+                  imageUrl:
+                    "https://chernivtsi.celentano.delivery/wp-content/uploads/2019/07/bavarska-445x445.jpg",
+                  items: [{ price: 550 }],
+                },
+                {
+                  id: 12,
+                  name: "Піца",
+                  imageUrl:
+                    "https://chernivtsi.celentano.delivery/wp-content/uploads/2019/07/bavarska-445x445.jpg",
+                  items: [{ price: 550 }],
+                },
+                {
+                  id: 13,
+                  name: "Піца",
+                  imageUrl:
+                    "https://chernivtsi.celentano.delivery/wp-content/uploads/2019/07/bavarska-445x445.jpg",
+                  items: [{ price: 550 }],
+                },
+              ]}
+              categoryId={3}
+            />
           </div>
         </div>
       </Container>
--- components/shared/categories.tsx
+++ components/shared/categories.tsx
@@ -1,4 +1,6 @@
+"use client";
 import { cn } from "@/lib/utils";
+import { useCategoryStore } from "@/store/category";
 import React from "react";
 
 type Props = {
@@ -6,17 +8,17 @@ type Props = {
 };
 
 const cats = [
-  "Піци",
-  "Комбо",
-  "Закуски",
-  "Коктейлі",
-  "Кава",
-  "Напої",
-  "Десерти",
+  { id: 1, name: "Піци" },
+  { id: 2, name: "Комбо" },
+  { id: 3, name: "Закуски" },
+  { id: 4, name: "Коктейлі" },
+  { id: 5, name: "Кава" },
+  { id: 6, name: "Напої" },
+  { id: 7, name: "Десерти" },
 ];
-const activeIndex = 0;
 
 export const Categories: React.FC<Props> = ({ className }) => {
+  const categoryActiveId = useCategoryStore((state) => state.activeId);
   return (
     <div
       className={cn("inline-flex gap-1 bg-gray-50 p-1 rounded-2xl", className)}
@@ -25,12 +27,13 @@ export const Categories: React.FC<Props> = ({ className }) => {
         <a
           className={cn(
             "flex items-center font-bold h-11 rounded-2xl px-5",
-            activeIndex == index &&
+            categoryActiveId == cat.id &&
               "bg-white shadow-md shadow-gray-200 text-primary"
           )}
+          href={`/#${cat.name}`}
           key={index}
         >
-          <button>{cat}</button>
+          <button>{cat.name}</button>
         </a>
       ))}
     </div>
